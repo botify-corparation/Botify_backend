b41800465c853fd1a5857f329b8bbd9e21fff224
Omadbek Tuxtasinboyev <omadbek1223erubdnx@gmail.com>
2025-08-13 12:47:37 +0500

    botni bir ikkita joyi tugirlab buldi

--- src/modules/controler-bot/controler-bot.service.ts
+++ src/modules/controler-bot/controler-bot.service.ts
@@ -1,5 +1,6 @@
 import { ConflictException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { UserRole } from '@prisma/client';
 import { PrismaService } from 'src/core/config/prisma/prisma.service';
 
 @Injectable()
@@ -17,11 +18,24 @@ export class ControlerBotService {
     const existsUser = await this.prisma.user.findUnique({ where: { id: userId } })
     if (!existsUser) throw new NotFoundException('user not found')
 
+    if (existsUser.role !== UserRole.ADMIN) {
+      const existsToken = await this.prisma.userBot.findUnique({ where: { botToken: token } })
+      if (existsToken) throw new ConflictException('this token already exist')
+      const existsPayment = await this.prisma.payment.findMany({ where: { userId } })
+      if (!existsPayment) throw new NotFoundException('this user won\'t sell still bot')
+      const creatUserBot = await this.prisma.userBot.create({
+        data: {
+          botToken: token,
+          categoryId: existsName.categoryId,
+          userId,
+          botModelId: existsName.id,
+          isActive: true,
+        }
+      })
+    }
     const existsToken = await this.prisma.userBot.findUnique({ where: { botToken: token } })
     if (existsToken) throw new ConflictException('this token already exist')
-      
-    const existsPayment = await this.prisma.payment.findMany({ where: { userId } })
-    if (!existsPayment) throw new NotFoundException('this user won\'t sell still bot')
+
     const creatUserBot = await this.prisma.userBot.create({
       data: {
         botToken: token,
