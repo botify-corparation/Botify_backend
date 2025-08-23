ece6e399db1b28c43901ad7aa5072d780099731e
Omadbek Tuxtasinboyev <omadbek1223erubdnx@gmail.com>
2025-08-23 12:09:28 +0500

    cors quhildi;

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:5173',
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   });
